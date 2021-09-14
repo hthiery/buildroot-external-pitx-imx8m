180acdcc20713f845c9bed70ae6c325562459257
Heiko Thiery <heiko.thiery@gmail.com>
2021-09-14 22:37:56 +0200

    imx8mm_n801x: add initial support
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

---
+++ board/kontron/imx8mm_n801x/boot.cmd
@@ -0,0 +1,8 @@
+echo "Root File Sytem on MMC${devnum}"
+setenv rootfs /dev/mmcblk${devnum}p1
+setenv bootargs root=${rootfs} rootwait rw
+
+load ${devtype} ${devnum} ${kernel_addr_r} boot/Image
+load ${devtype} ${devnum} ${fdt_addr_r} boot/pitx-imx8m_hdmi.dtb
+
+booti ${kernel_addr_r} - ${fdt_addr_r}
