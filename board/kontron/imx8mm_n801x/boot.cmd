26275cbdde3914889ace9101a681074c001e4b6d
Heiko Thiery <heiko.thiery@gmail.com>
2021-09-15 14:44:22 +0200

    imx8mm_n801x_s: fix boot.scr
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

--- board/kontron/imx8mm_n801x/boot.cmd
+++ board/kontron/imx8mm_n801x/boot.cmd
@@ -3,6 +3,6 @@ setenv rootfs /dev/mmcblk${devnum}p1
 setenv bootargs root=${rootfs} rootwait rw
 
 load ${devtype} ${devnum} ${kernel_addr_r} boot/Image
-load ${devtype} ${devnum} ${fdt_addr_r} boot/pitx-imx8m_hdmi.dtb
+load ${devtype} ${devnum} ${fdt_addr_r} boot/imx8mm-kontron-n801x-s.dtb
 
 booti ${kernel_addr_r} - ${fdt_addr_r}
